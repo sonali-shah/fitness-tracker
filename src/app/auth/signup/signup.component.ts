dbf461acf0cb423bf9d92531d3dc1fb901839f08
Sonali Ronak Shah <sonali.shah@gmx.de>
2023-11-08 16:21:24 +0100

    Added spinner on sign up and loading exercises in new training component

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,19 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Form, NgForm } from '@angular/forms';
 import { AuthService } from '../auth.service';
+import { UIService } from 'src/app/shared/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent implements OnInit {
+export class SignupComponent implements OnInit, OnDestroy {
 
   maxDate: Date = new Date();
+  isLoading: boolean = false;
+  loadingSubs?: Subscription;
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private uiService: UIService) {}
 
   ngOnInit() {
+
+    this.loadingSubs = this.uiService.loadingStateChanged.subscribe((isLoading) => {
+      this.isLoading = isLoading;
+    })
+
     this.maxDate = new Date();
     this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
   }
@@ -23,6 +32,10 @@ export class SignupComponent implements OnInit {
       email: form.value.email,
       password: form.value.password
     });
+  }
+
+  ngOnDestroy(): void {
+    this.loadingSubs?.unsubscribe();
   }
 
 }
--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,28 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { TrainingService } from '../training.service';
 import { Exercise } from '../exercise.model';
 import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-new-training',
   templateUrl: './new-training.component.html',
   styleUrls: ['./new-training.component.css']
 })
-export class NewTrainingComponent implements OnInit{
+export class NewTrainingComponent implements OnInit, OnDestroy {
   availableExercises: Exercise[] = [];
+  isLoading: boolean = false;
+  avlExerciseChangedSubs?: Subscription;
 
   constructor(private trainingService: TrainingService) {
   }
   
   ngOnInit(): void {
+    this.isLoading = true;
     this.trainingService.fetchAvailableExercises();
-    this.trainingService.availableExercisesChanged.subscribe(() => {
+    this.avlExerciseChangedSubs = this.trainingService.availableExercisesChanged.subscribe(() => {
+      this.isLoading = false;
       this.availableExercises = this.trainingService.getAvailableExercises();
     })
   }
 
   startTraining(f: NgForm) {
-    console.log(f);
     this.trainingService.startExercise(f.value.exercise);
+  }
+
+  ngOnDestroy(): void {
+    this.avlExerciseChangedSubs?.unsubscribe();
   }
 }
